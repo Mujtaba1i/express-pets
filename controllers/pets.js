3b8bf4c69a7b34c409691d063b1fb85f30b5fc56
Mujtaba1i <unmujt@gmail.com>
2025-12-28 15:54:03 +0300

    quick commit

--- controllers/pets.js
+++ controllers/pets.js
@@ -2,9 +2,26 @@ const Pet = require('../models/pet')
 const express = require('express')
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+    try {
+        const allPets = await Pet.find()
+        res.status(200).json(allPets)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({err: 'Failed to Fetch Data'})        
+    }
+})
+
+
 router.post('/', async (req, res) => {
-    await Pet.create(req.body)
-    res.status(201).send()
+    try {
+        const creatdPet = await Pet.create(req.body)
+        res.status(201).json(creatdPet)        
+    } catch (err) {
+        console.log(err.message)
+        res.status(422).json({err: 'Failed to Create'})
+    }
+
 })
 
 module.exports = router;
