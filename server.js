1445c322a9b8a6c5b6a0134321be9d57abc67c81
Mujtaba1i <unmujt@gmail.com>
2025-12-28 10:01:06 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+
+try{
+    mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connection.on('connected', () => console.log(`Connected to MongoDB ${mongoose.connection.name}`));
+}
+catch(err){
+    console.log('Ran into an error: ' +err)
+}
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes go here
+
+
+
+
+app.listen(3000, () => {
+  console.log('Listing on port 3000');
+});
